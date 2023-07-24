379e06d23dd0b46b3c1bdc4c2794a48426988107
Shmouel kessous <chmouel2690@gmail.com>
2023-07-24 18:01:52 +0300

    init + login registerUI +prisma

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5 flex flex-col items-center">
+		<h1 class="h1">{$page.status}</h1>
+		<h3 class="h3">{$page.error?.message}</h3>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div dir="rtl" class="space-y-5">
+		<h1 class="h1">ברוכים הבאים ל-MagicList</h1>
+		<p>רשימת קניות חכמה ואינטראקטיבית.</p>
+
+		<div class="w-full flex justify-center pt-5">
+			<a href="/auth/login">
+				<button type="button" class=" btn variant-filled-primary flex items-center justify-between">
+					<span>התחלה</span>
+					<span><iconify-icon icon="formkit:arrowleft" /></span>
+				</button>
+			</a>
+		</div>
+	</div>
+</div>
